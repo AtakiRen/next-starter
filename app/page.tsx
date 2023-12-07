389deab5dd6d9bfd1185a268cfb9a6488734f684
AtakiRen <hoangnam1772004@gmail.com>
2023-12-07 19:11:26 +0700

    first init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+      <h2 className='text-yellow-700 text-center text-2xl font-bold '>Hello World</h2>  
+    </main>
+  )
+}
